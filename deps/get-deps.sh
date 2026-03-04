2ef62fb60b0e62e9f654cb9683bfc3edf354a58d
Ken Kan <kenhkan@gmail.com>
2026-03-03 22:18:47 -0500

    Do not remove downloaded artifacts for backup and backtracing

--- deps/get-deps.sh
+++ deps/get-deps.sh
@@ -97,15 +97,12 @@ if [ "$SKIP_DOWNLOAD" = 0 ] ; then
       if [ "$USE_NIGHTLY" = 1 ] ; then
         wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-macos-latest.zip
         unzip libtorch-macos-latest.zip
-        rm libtorch-macos-latest.zip
       elif [ "$USE_BINARY_FOR_CI" = 1 ] ; then
         wget https://github.com/hasktorch/libtorch-binary-for-ci/releases/download/${VERSION}/cpu-libtorch-macos-latest.zip
         unzip cpu-libtorch-macos-latest.zip
-        rm cpu-libtorch-macos-latest.zip
       else
         wget -O libtorch-macos.zip https://download.pytorch.org/libtorch/cpu/libtorch-macos-arm64-${VERSION}.zip
         unzip libtorch-macos.zip
-        rm libtorch-macos.zip
       fi
       wget https://github.com/hasktorch/tokenizers/releases/download/libtokenizers-v0.1/libtokenizers-macos.zip
       unzip libtokenizers-macos.zip
@@ -114,11 +111,9 @@ if [ "$SKIP_DOWNLOAD" = 0 ] ; then
       if [ "$USE_NIGHTLY" = 1 ] ; then
         wget https://download.pytorch.org/libtorch/nightly/${COMPUTE_ARCH}/libtorch-cxx11-abi-shared-with-deps-latest.zip
         unzip libtorch-cxx11-abi-shared-with-deps-latest.zip
-        rm libtorch-cxx11-abi-shared-with-deps-latest.zip
       elif [ "$USE_BINARY_FOR_CI" = 1 ] ; then
         wget https://github.com/hasktorch/libtorch-binary-for-ci/releases/download/${VERSION}/${COMPUTE_ARCH}-libtorch-cxx11-abi-shared-with-deps-latest.zip
         unzip ${COMPUTE_ARCH}-libtorch-cxx11-abi-shared-with-deps-latest.zip
-        rm ${COMPUTE_ARCH}-libtorch-cxx11-abi-shared-with-deps-latest.zip
       else
 	case "${COMPUTE_ARCH}" in
 	      "cpu" )   URL=https://download.pytorch.org/libtorch/${COMPUTE_ARCH}/libtorch-cxx11-abi-shared-with-deps-${VERSION}%2Bcpu.zip ;;
@@ -130,7 +125,6 @@ if [ "$SKIP_DOWNLOAD" = 0 ] ; then
 	esac
 	wget -O libtorch-cxx11-abi-shared-with-deps-${VERSION}.zip "$URL"
         unzip libtorch-cxx11-abi-shared-with-deps-${VERSION}.zip
-        rm libtorch-cxx11-abi-shared-with-deps-${VERSION}.zip
       fi
 
       wget https://github.com/hasktorch/tokenizers/releases/download/libtokenizers-v0.1/libtokenizers-linux.zip
